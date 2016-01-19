f4936f0609c7d32099a6f9c7aaeec092a5a17209
Bernd Toepfer <bernd@bc-toepfer.de>
2016-01-19 18:53:05 +0100

    Internationalisierung implementiert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :except => [:index]
+  before_action :set_locale
+ 
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
@@ -12,6 +14,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :language) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :language) }
+  end
+  
+  def set_locale
+    I18n.locale = extract_locale_from_user || I18n.default_locale
+  end
+  
+  def extract_locale_from_user
+    if user_signed_in?
+      current_user.language
+    end
   end
   
 end
