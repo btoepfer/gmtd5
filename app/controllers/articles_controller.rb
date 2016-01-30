34f71ac2caef74d093ab3b2e3e4190b9afcc32e3
Bernd Toepfer <bernd@bc-toepfer.de>
2016-01-30 23:29:45 +0100

    content-Suche implementiert

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,18 @@ class ArticlesController < ApplicationController
     like_or_equal = "="
     if params[:s].present?
       @keywords = params[:s]
+      
+      # Enthält der Suchbegriff ein "*", dann suchen wir mit "like"
       like_or_equal = "like" if @keywords.include? '*'
-      where_clause = ["user_id = ? and upper(title) #{like_or_equal} ?", current_user.id, @keywords.upcase.gsub('*','%')]
+      
+      title_search = @keywords.upcase.gsub('*','%')
+      content_search = "%"+title_search+"%"
+      
+      # upcase, damit Groß-/Kleinschreibung ignoriert wird, "*" wird durch "%" ersetzt
+      where_clause = ["user_id = ? and (upper(title) #{like_or_equal} ? or upper(content) like ?)", current_user.id, title_search, content_search]
+      
       @articles = Article.where(where_clause)
+      
     else
       @articles = []
     end
