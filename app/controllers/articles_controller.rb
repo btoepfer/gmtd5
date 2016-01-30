ea745200b0cf667d1729b761ed268d6b24248607
Bernd Toepfer <bernd@bc-toepfer.de>
2016-01-30 20:16:43 +0100

    Groß-/Kleinschreibung ignorieren

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
     if params[:s].present?
       @keywords = params[:s]
       like_or_equal = "like" if @keywords.include? '*'
-      where_clause = ["user_id = ? and title #{like_or_equal} ?", current_user.id, @keywords.gsub('*','%')]
+      where_clause = ["user_id = ? and upper(title) #{like_or_equal} ?", current_user.id, @keywords.upcase.gsub('*','%')]
       @articles = Article.where(where_clause)
     else
       @articles = []
