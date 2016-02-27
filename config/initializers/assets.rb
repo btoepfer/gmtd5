88dd5bc73737d54e34b24e36679800333c9cfff9
Bernd Toepfer <bernd@bc-toepfer.de>
2016-02-27 18:14:53 +0100

    fontawesome korrigiert

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join("vendor","assets","bowe
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( GmTD50.svg )
-Rails.application.config.assets.precompile << %r(fonts/[\w-]+\.(?:eot|svg|ttf|woff)$)
+# Rails.application.config.assets.precompile << %r(fonts/[\w-]+\.(?:eot|svg|ttf|woff)$)
