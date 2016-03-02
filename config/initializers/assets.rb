6afe49442cfc6d37741395149c91e23f89bea51f
Bernd Toepfer <bernd@bc-toepfer.de>
2016-03-02 17:53:30 +0100

    Fontawesome Fonts in public-Verzeichnis kopiert

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.1'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-Rails.application.config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+#Rails.application.config.assets.paths << Rails.root.join("vendor","assets","bower_components")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
